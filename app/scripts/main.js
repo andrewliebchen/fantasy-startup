ca3ab75d8522d0cd0efe2528eee5a0dd8aad2a7b
andrewliebchen <andrewliebchen@gmail.com>
2014-11-09 21:16:37 -0800

    Basic panel toggle

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,15 @@
-console.log('\'Allo \'Allo!');
+$(document).ready(function() {
+  $(document).on('click', '[data-toggle="panel"]', function(event) {
+    event.preventDefault();
+    $('body').toggleClass('show-panel');
+
+    $('.main').on('click', '.main', function(event){
+      $('body').removeClass('show-panel');
+    });
+  });
+
+  // make this more specific
+  $(document).on('click', 'button', function(event) {
+    event.stopPropagation();
+  });
+});
