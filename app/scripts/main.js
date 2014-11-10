cdcd7777703338fe6cd4b9d4648e9858fc197d26
andrewliebchen <andrewliebchen@gmail.com>
2014-11-09 22:44:03 -0800

    Panel click catcher

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,10 +2,10 @@ $(document).ready(function() {
   $(document).on('click', '[data-toggle="panel"]', function(event) {
     event.preventDefault();
     $('body').toggleClass('show-panel');
+  });
 
-    $('.main').on('click', '.main', function(event){
-      $('body').removeClass('show-panel');
-    });
+  $(document).on('click', '.panel__background', function(){
+    $('body').removeClass('show-panel');
   });
 
   // make this more specific
