bc7399f2d888792d200d35654db6ed1f493bd4aa
andrewliebchen <andrewliebchen@gmail.com>
2014-11-13 23:35:24 -0800

    Make the panel work, whoo hoo!

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,9 +1,27 @@
 $(document).ready(function() {
-  $(document).on('click', '[data-toggle="panel"]', function(event) {
-    event.preventDefault();
-    $('body').toggleClass('show-panel');
-  });
+  // Use tabletop to get the the startup info
+  var URL = "1orGnmev3T59ybT9WkKtzlmuLERL-QYy_3dQgNlVlIpw";
+  Tabletop.init( { key: URL, callback: showStartups, simpleSheet: true } );
 
+  function showStartups(data) {
+    // Populate the startup table
+    var source   = $("#startups_template").html();
+    var template = Handlebars.compile(source);
+    $("#startups").html(template({row: data}));
+
+    // Open the panel
+    $(document).on('click', '[data-toggle="panel"]', function(event) {
+      event.preventDefault();
+      var rowID = $(this).data('startup-id') - 1;
+      var source   = $("#startup_detail_template").html();
+      var template = Handlebars.compile(source);
+      $("#startup_detail").html(template(data[rowID]));
+
+      $('body').toggleClass('show-panel');
+    });
+  }
+
+  // Close the panel when click outside panel
   $(document).on('click', '.panel__background', function(){
     $('body').removeClass('show-panel');
   });
