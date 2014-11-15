a29109b49a1ab94b8f1592598a2550cf000ce835
andrewliebchen <andrewliebchen@gmail.com>
2014-11-15 15:50:35 -0800

    Stealth mode and support multiple sheets

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,21 +1,27 @@
 $(document).ready(function() {
   // Use tabletop to get the the startup info
   var URL = "1orGnmev3T59ybT9WkKtzlmuLERL-QYy_3dQgNlVlIpw";
-  Tabletop.init( { key: URL, callback: showStartups, simpleSheet: true } );
+  Tabletop.init({
+    key: URL,
+    callback: showStartups,
+    simpleSheet: false
+  });
 
-  function showStartups(data) {
+  function showStartups(data, tabletop) {
     // Populate the startup table
-    var source   = $("#startups_template").html();
-    var template = Handlebars.compile(source);
-    $("#startups").html(template({row: data}));
+    var startupsData = tabletop.sheets('Startups').all();
+    var globalData  = tabletop.sheets('global').all();
+    var source       = $("#startups_template").html();
+    var template     = Handlebars.compile(source);
+    $("#startups").html(template({row: startupsData}));
 
     // Open the panel
     $(document).on('click', '[data-toggle="panel"]', function(event) {
       event.preventDefault();
-      var rowID = $(this).data('startup-id') - 1;
+      var rowID    = $(this).data('startup-id') - 1;
       var source   = $("#startup_detail_template").html();
       var template = Handlebars.compile(source);
-      $("#startup_detail").html(template(data[rowID]));
+      $("#startup_detail").html(template(startupsData[rowID]));
 
       $('body').toggleClass('show-panel');
     });
