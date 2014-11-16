df954cbccbda5f4bf8ae0d1609f6ad58d12b8314
andrewliebchen <andrewliebchen@gmail.com>
2014-11-16 00:22:17 -0800

    Fix JS linting errors

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,32 +1,37 @@
+'use strict';
+
 $(document).ready(function() {
-  // Use tabletop to get the the startup info
-  // Source spreadsheet: https://docs.google.com/spreadsheets/d/1orGnmev3T59ybT9WkKtzlmuLERL-QYy_3dQgNlVlIpw/edit#gid=0
-  var URL = "1orGnmev3T59ybT9WkKtzlmuLERL-QYy_3dQgNlVlIpw";
-  Tabletop.init({
-    key: URL,
-    callback: showStartups,
-    simpleSheet: false
-  });
+  var Handlebars;
+  var Tabletop;
 
   function showStartups(data, tabletop) {
     // Populate the startup table
     var startupsData = tabletop.sheets('Startups').all();
-    var globalData  = tabletop.sheets('global').all();
-    var source       = $("#startups_template").html();
+    // var globalData   = tabletop.sheets('global').all();
+    var source       = $('#startups_template').html();
     var template     = Handlebars.compile(source);
-    $("#startups").html(template({row: startupsData}));
+    $('#startups').html(template({row: startupsData}));
 
     // Open the panel
     $(document).on('click', '[data-toggle="panel"]', function(event) {
       event.preventDefault();
       var rowID    = $(this).data('startup-id') - 1;
-      var source   = $("#startup_detail_template").html();
+      var source   = $('#startup_detail_template').html();
       var template = Handlebars.compile(source);
-      $("#panel__contents").html(template(startupsData[rowID]));
+      $('#panel__contents').html(template(startupsData[rowID]));
 
       $('body').addClass('show-panel');
     });
   }
+
+  // Use tabletop to get the the startup info
+  // Source spreadsheet: https://docs.google.com/spreadsheets/d/1orGnmev3T59ybT9WkKtzlmuLERL-QYy_3dQgNlVlIpw/edit#gid=0
+  var URL = '1orGnmev3T59ybT9WkKtzlmuLERL-QYy_3dQgNlVlIpw';
+  Tabletop.init({
+    key: URL,
+    callback: showStartups,
+    simpleSheet: false
+  });
 
   // Close the panel when click outside panel
   $(document).on('click', '.overlay__background', function(){
